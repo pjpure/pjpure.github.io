015e4bd2116f2c4e15bcff6c70f92d0f6c684943
pjpure <pure254326@gmail.com>
2021-10-26 02:50:02 +0700

    add react

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "200px" }}>
+      <h1>Pattapon Janchoo</h1>
+    </div>
+  );
+}
+
+export default HomePage;
