69eb54bfd06a4c64b6bde40beb3585f2e9c7ce4f
pjpure <pure254326@gmail.com>
2022-01-06 20:32:45 +0700

    update

--- src/components/Contents/About/About.js
+++ src/components/Contents/About/About.js
@@ -33,13 +33,13 @@ function About() {
                 </Col>
                 <Col style={{ marginBottom: "14px" }} lg="12">
                   <p>
-                    Hi, My name is Pattapon Janchoo. I am a computer engineering
-                    student at King Mongkut's Institute of Technology
-                    Ladkrabang. I am interested in software development and
-                    innovations that will make everything easier. I want to
-                    learn how to work as a software developer in a real company.
-                    I intended to develop my skills and learn something new
-                    every day from your company.
+                    I am a third year computer engineering student at King
+                    Mongkut's Institute of Technology Ladkrabang. I am
+                    interested in software development and innovations that will
+                    make everything easier. I want to learn how to work as a
+                    software developer in a real company. I intended to develop
+                    my skills and learn something new every day from your
+                    company.
                   </p>
                 </Col>
 
